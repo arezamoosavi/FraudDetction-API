c453bcdaed7f9c29c933e43a81d67f54b9f74671
alimo <sdamoosavi@gmail.com>
2020-06-23 12:21:21 +0430

    websocker and restAPI works!

--- web_api/main/app/api.py
+++ web_api/main/app/api.py
@@ -1,18 +1,63 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, WebSocket
+from fastapi.responses import HTMLResponse
+from joblib import load
+
+clf = load("app/forest_model.joblib")
 
 router = APIRouter()
 
 
-@router.get("/users/", tags=["users"])
-async def read_users():
-    return [{"username": "Foo"}, {"username": "Bar"}]
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://localhost:8000/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
 
 
-@router.get("/users/me", tags=["users"])
-async def read_user_me():
-    return {"username": "fakecurrentuser"}
+@router.get("/wspred", tags=["websocket"])
+async def get():
+    return HTMLResponse(html)
 
 
-@router.get("/users/{username}", tags=["users"])
-async def read_user(username: str):
-    return {"username": username}
+@router.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        ndata = [float(number) for number in data.split(",")]
+
+        result = clf.predict([ndata])
+        if result[0] == 0:
+            response = "Not Fraud!"
+        else:
+            response = "A Fraud!"
+
+        await websocket.send_text(f"Message text was: {response}")
